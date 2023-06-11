babb86de027a698452047d05c7d0c2da4382d61a
Gavin Burnell <g.burnell@leeds.ac.uk>
2023-06-11 17:52:40 +0100

    Switch to using postgresql as the stadndard database

--- project_name/settings/development.py
+++ project_name/settings/development.py
@@ -3,6 +3,7 @@ from os.path import join
 
 # project imports
 from .common import *
+from secrets import DATABASES
 
 # uncomment the following line to include i18n
 # from .i18n import *
@@ -19,20 +20,6 @@ LOGIN_URL = 'core_login'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = 'core_login'
 
-
-# ##### DATABASE CONFIGURATION ############################
-DATABASES = {
-    'default': {
-    #Settings for mysql on a centos server communicating locally over a unix port
-    #Assumes DB and DB-user are both the site name - which is vaguely sensible.
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': SITE_NAME,
-        'USER': SITE_NAME,
-        'PASSWORD':'[[db password]]',
-        'HOST':'/var/lib/mysql/mysql.sock',
-        'PORT':'',
-    }
-}
 
 # ##### APPLICATION CONFIGURATION #########################
 
